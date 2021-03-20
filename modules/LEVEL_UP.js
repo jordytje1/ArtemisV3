1e8630f6a09f74a00c60f6072cae2b5bd50d1723
UtopicUnicorns <30652988+UtopicUnicorns@users.noreply.github.com>
2021-03-20 14:26:12 +0100

    V3 bug fixes
    Some bug squashing

--- modules/LEVEL_UP.js
+++ modules/LEVEL_UP.js
@@ -4,10 +4,12 @@
 ////////////////////////////////////
 module.exports = {
   eventTrigger: async function (client, CONFIG, npm, mmbr, msg, snd, gld) {
-    score = await getScore.get(mmbr.user.id, msg.guild_id);
+    const redoMember = await gld.members.cache.get(msg.author.id);
+
+    const score = await getScore.get(redoMember.user.id, msg.guild_id);
     if (!score) return;
 
-    getLevels = await db
+    const getLevels = await db
       .prepare(`SELECT * FROM levelup WHERE guildid = ? ORDER BY "level" ASC`)
       .all(`${gld.id}`);
     if (!getLevels[0]) return;
@@ -15,12 +17,12 @@ module.exports = {
     for (let i of getLevels) {
       if (!score) return;
       if (score.level >= i.level) {
-        let checking = await mmbr.roles.cache.has(i.role);
+        let checking = await redoMember.roles.cache.has(i.role);
         if (!checking) {
           let check2 = await gld.roles.cache.find((r) => r.id == `${i.role}`);
           if (check2) {
             try {
-              await mmbr.roles.add(check2);
+              await redoMember.roles.add(check2);
 
               let embed = new Discord.MessageEmbed()
                 .setThumbnail(
